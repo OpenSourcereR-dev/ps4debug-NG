3c94b891c23d3aac6d36a4bd46033a863b0ada6b
OpenSourcereR-dev <45182707+OpenSourcereR-dev@users.noreply.github.com>
2026-05-07 19:17:45 +0700

    Relocate ptrace and breakpoint logic from kern to debug module
    Move the ptrace wrappers, breakpoint table, and debug stop/step/resume
    handlers out of kern.c into debug.c so kern.c can focus on kernel-level
    primitives. Headers are split accordingly and kern.c now pulls in kdbg.h
    for the kernel debug surface that remains.

--- debugger/include/console.h
+++ debugger/include/console.h
@@ -5,12 +5,10 @@
 #include <ps4.h>
 #include "protocol.h"
 #include "net.h"
-#include "debug.h"
 
-int kern_base_handle(int fd, struct cmd_packet *packet);
-int kern_read_handle(int fd, struct cmd_packet *packet);
-int kern_write_handle(int fd, struct cmd_packet *packet);
-
-int kern_handle(int fd, struct cmd_packet *packet);
+int console_handle(int fd, struct cmd_packet *packet);
+int console_reboot_handle(int fd, struct cmd_packet *packet);
+int console_print_handle(int fd, struct cmd_packet *packet);
+int console_notify_handle(int fd, struct cmd_packet *packet);
 
 #endif
--- debugger/source/console.c
+++ debugger/source/console.c
@@ -1,62 +1,83 @@
 
 #include "console.h"
-#include "kdbg.h"
+#include "debug.h"
 
-int kern_base_handle(int fd, struct cmd_packet *packet) {
-    uint64_t kbase = 0;
-    sys_kern_base(&kbase);
-    net_send_int32(fd, CMD_SUCCESS);
-    net_send_all(fd, &kbase, 8);
-    return 0;
-}
+int console_print_handle(int fd, struct cmd_packet *packet) {
+    uint32_t *lenp;
+    uint32_t length;
+    void *data;
 
-int kern_read_handle(int fd, struct cmd_packet *packet) {
-    struct cmd_kern_read_packet *rp = (struct cmd_kern_read_packet *)packet->data;
-    if (!rp) {
+    lenp = (uint32_t *)packet->data;
+    if (!lenp) {
         net_send_int32(fd, CMD_DATA_NULL);
-        return 0;
+        return 1;
     }
 
-    void *data = net_alloc_buffer(rp->length);
+    length = *lenp;
+    data = net_alloc_buffer(length);
     if (!data) {
         net_send_int32(fd, CMD_DATA_NULL);
         return 1;
     }
 
-    sys_kern_rw(rp->address, data, rp->length, 0);
-
+    memset(data, NULL, length);
+    net_recv_all(fd, data, length, 1);
+    syscall(112, 2, data);
     net_send_int32(fd, CMD_SUCCESS);
-    net_send_all(fd, data, rp->length);
     free(data);
     return 0;
 }
 
-int kern_write_handle(int fd, struct cmd_packet *packet) {
-    struct cmd_kern_write_packet *wp = (struct cmd_kern_write_packet *)packet->data;
-    if (!wp) {
+int console_notify_handle(int fd, struct cmd_packet *packet) {
+    struct cmd_console_notify_packet *np;
+    void *data;
+
+    np = (struct cmd_console_notify_packet *)packet->data;
+    if (!np) {
         net_send_int32(fd, CMD_DATA_NULL);
-        return 0;
+        return 1;
     }
 
-    void *data = net_alloc_buffer(wp->length);
+    data = net_alloc_buffer(np->length);
     if (!data) {
         net_send_int32(fd, CMD_DATA_NULL);
         return 1;
     }
 
-    net_send_int32(fd, CMD_SUCCESS);
-    net_recv_all(fd, data, wp->length, 1);
-    sys_kern_rw(wp->address, data, wp->length, 1);
+    memset(data, NULL, np->length);
+    net_recv_all(fd, data, np->length, 1);
+    sceSysUtilSendSystemNotificationWithText(np->messageType, data);
     net_send_int32(fd, CMD_SUCCESS);
     free(data);
     return 0;
 }
 
-int kern_handle(int fd, struct cmd_packet *packet) {
-    switch (packet->cmd) {
-        case CMD_KERN_BASE:   return kern_base_handle(fd, packet);
-        case CMD_KERN_READ:   return kern_read_handle(fd, packet);
-        case CMD_KERN_WRITE:  return kern_write_handle(fd, packet);
+int console_reboot_handle(int fd, struct cmd_packet *packet) {
+    if (g_debugging) {
+        debug_cleanup(curdbgctx);
+        sceNetSocketClose(fd);
     }
+    syscall(112, 1, 0);
+
+    return 1;
+}
+
+int console_handle(int fd, struct cmd_packet *packet) {
+    switch(packet->cmd) {
+        case CMD_CONSOLE_REBOOT:
+            return console_reboot_handle(fd, packet);
+        case CMD_CONSOLE_PRINT:
+            return console_print_handle(fd, packet);
+        case CMD_CONSOLE_NOTIFY:
+            return console_notify_handle(fd, packet);
+        case CMD_CONSOLE_INFO:
+
+            net_send_int32(fd, CMD_SUCCESS);
+            return 0;
+        case CMD_CONSOLE_END:
+
+            return 1;
+    }
+
     return 1;
 }
